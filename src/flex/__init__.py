5d6c99b1d14d16e31da8b7ec4931431301b10d08
michael-petersen <petersen.michael.s@gmail.com>
2025-08-02 14:19:50 +0100

    first commit

---
+++ src/flex/__init__.py
@@ -0,0 +1,13 @@
+"""
+flex
+
+Fourier-Laguerre basis function expansions
+
+See README file and online documentation (https://flexable.readthedocs.io)
+for further details and usage instructions.
+"""
+from .flexbase import FLEX
+from importlib.metadata import version
+
+__version__ = version("flex")
+__all__ = ["FLEX"]]
