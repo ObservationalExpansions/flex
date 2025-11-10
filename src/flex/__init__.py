ac2d93cba576c1fbfa5acf8453bb89463a2db67e
michael-petersen <petersen.michael.s@gmail.com>
2025-11-10 15:53:14 +0000

    simplify base flex structure

--- src/flex/__init__.py
+++ src/flex/__init__.py
@@ -7,9 +7,7 @@ See README file and online documentation (https://flexable.readthedocs.io)
 for further details and usage instructions.
 """
 from .flexbase import FLEX
-from .flexnumba import FLEXY
-from .flexcython import FLEXC
 from importlib.metadata import version
 
 __version__ = version("flex")
-__all__ = ["FLEX", "FLEXY", "FLEXC"]
+__all__ = ["FLEX"]
